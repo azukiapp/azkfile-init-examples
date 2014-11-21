99752b757a8e3c731e238bf1fe73d13dd0a05302
Julio Makdisse Saito <saitodisse@gmail.com>
2014-11-21 19:05:07 -0200

    Adding Flask project

---
+++ flask/server.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+PORT = int(os.getenv('HTTP_PORT'))
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=True)
